aca366409afc056b2c1eabd05822086fde129cfd
ayushprobroyopython <ayushprobroyoking@gmail.com>
2024-12-24 11:36:21 +0530

    Add files via upload

---
+++ prooo.py
@@ -0,0 +1,11 @@
+a = input('What country to nuke: ')# Which country to nuke 
+b = input('How many nukes to send:  ')# How many nukes to send
+print (a + (' is nuked with ') + b +  (' nukes'))
+c = input((a) + (' has defended itself. Do you wish to start a war [y or n]: '))# Asking to start a war
+if (c== ('y')):
+    print('You won!')
+    print('India Zindabad')
+    print ((a) + (' is a failure'))
+else:
+    print ('You are a dissapointment')
+
